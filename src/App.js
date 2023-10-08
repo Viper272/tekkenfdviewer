7fc38a904fb03ac7e2272d641800a833df93aba1
Viper272 <alexmiller968@gmail.com>
2023-10-08 22:08:31 +1300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from 'react';
+import {View} from 'react-native-web';
+
+import MoveAttack from './components/moveAttack';
+
+function App() {
+  return (
+    <View>
+      {/* <Text>Hello. I'm a Cat!</Text> */}
+      <MoveAttack/>
+    </View>
+  );
+}
+
+export default App;
